e469c0e9b1f3169963a23624ba2d71117815d105
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-01-27 20:35:16 +0300

    feat: create simple counter of number

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  React.createElement('button', {
-    disabled: true
-  }, 'Нажми на меня'),
+  <App/>,
   document.getElementById('root')
 );
