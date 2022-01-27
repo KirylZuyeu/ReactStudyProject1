f419c9f3a92a1c6942f7ec3cae7f9c090f75e2bd
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-01-28 00:34:31 +0300

    feat: double way autentification

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState(0);
 
   function increment() {
-    setLikes(likes + 1)
+    setLikes(likes + 1);
   }
 
   function decrement() {
-    setLikes(likes - 1)
+    setLikes(likes - 1);
   }
 
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
