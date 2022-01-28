86e5e9adfa7546f2f081f2f7a55ee8a61c10df14
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-01-28 12:03:09 +0300

    feat: add list of post Items

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,13 +6,22 @@ import './styles/App.css'
 
 function App() {
 
-  const [value, setValue] = useState(0);
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript1', body: 'Description1'},
+    {id: 2, title: 'JavaScript2', body: 'Description2'},
+    {id: 3, title: 'JavaScript3', body: 'Description3'},
+    {id: 4, title: 'JavaScript4', body: 'Description4'},
+    {id: 5, title: 'JavaScript5', body: 'Description5'},
+  ]);
 
   
 
   return (
     <div className="App">
       <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}} />
+        {posts.map(post => 
+          <PostItem post={post} key={post.id}/>
+        )}
       <Counter/>
       <ClassCounter/>     
     </div>
