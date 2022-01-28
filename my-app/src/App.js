8f8daaad00effb23427c95f9c37400a3614dd0f0
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-01-28 12:21:24 +0300

    feat: add component of button with styles and porps children as name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,30 +3,30 @@ import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 import './styles/App.css'
 
 function App() {
   const [posts1, setPosts1] = useState([
     {id: 1, title: 'JavaScript1', body: 'Description1'},
     {id: 2, title: 'JavaScript2', body: 'Description2'},
-    {id: 3, title: 'JavaScript3', body: 'Description3'},
-    {id: 4, title: 'JavaScript4', body: 'Description4'},
-    {id: 5, title: 'JavaScript5', body: 'Description5'},
   ]);
 
   const [posts2, setPosts2] = useState([
     {id: 1, title: 'Pyton1', body: 'Description1'},
     {id: 2, title: 'Pyton2', body: 'Description2'},
-    {id: 3, title: 'Pyton3', body: 'Description3'},
-    {id: 4, title: 'Pyton4', body: 'Description4'},
-    {id: 5, title: 'Pyton5', body: 'Description5'},
   ]);
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}} />
+      <form>
+        <input type="text" placeholder="Name of the post"/>
+        <input type="text" placeholder="Description of the post"/>
+        <MyButton>Create Post</MyButton>
+      </form>
       <PostList posts={posts1} title='JavaScript List'/>
       <PostList posts={posts2} title='Pyton List'/>
+      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}} />
       <Counter/>
       <ClassCounter/>     
     </div>
